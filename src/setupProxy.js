69f9444581cc2c02ae2f875f36238446f899c67c
brave xue <brave.xue@hexagon.com>
2020-05-25 22:39:49 +0800

    change the proxy config

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,13 +2,13 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    "/webapi_20200217",
+    "/WebAPI",
     // "/webapi",
     proxy({
       target: "http://39.98.108.23/",
       changeOrigin: true,
       router: {
-        "http://192.168.1.6:3000": "http://39.98.108.23/"
+        "http://192.168.0.102:3000": "http://39.98.108.23/"
       },
       pathRewrite: {
         "^/api": ""
